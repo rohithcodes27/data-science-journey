b6378f8a0f7dd8943ba666452e11ca3de5ccb9b0
rohithcodes27 <rohithmudhalkar@gmail.com>
2026-02-10 15:14:48 +0530

    Practice pandas head, tail, filtering

---
+++ Python/pandas_basics.py
@@ -0,0 +1,19 @@
+Python
+
+# View first and last rows
+print("\nFirst 2 rows:")
+print(df.head(2))
+
+print("\nLast 2 rows:")
+print(df.tail(2))
+
+# Select specific columns 
+
+name_marks = df[["Name" , "Marks"]]
+print("\nName and Marks:")
+print(name_marks)
+
+# Filter using condition
+high_scores = df[df["Marks"] > 80]
+print("\nStudents with Marks > 80:")
+print(high_scorers)
