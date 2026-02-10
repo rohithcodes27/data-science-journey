3621cb94c5c9faac3d6627f8d54fd8c67df9d878
rohithcodes27 <rohithmudhalkar@gmail.com>
2026-02-10 12:11:27 +0530

    Create python basics.py

---
+++ Python/python basics.py
@@ -0,0 +1,19 @@
+# python basics for Data Science
+
+# Print statement 
+print("hello, Data Science!")
+
+# Variables
+name = "rohith"
+age = 21
+is_student = True
+
+print(name, age, is_student)
+
+# Data types
+numbers = [1, 2, 3, 4, 5]
+print(numbers)
+
+# Loop
+for n in numbers:
+  print(n * 2)
